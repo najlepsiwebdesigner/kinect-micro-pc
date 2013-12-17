6a08443258eea1296ac0647403be63d808c988e5
Peter Beno <najlepsiwebdesigner@gmail.com>
2013-12-17 11:24:32 +0000

    openni instalation & compilation alpha

--- setup_cubie.sh
+++ setup_cubie.sh
@@ -1,13 +1,18 @@
- #!/bin/sh
+#!/bin/sh
  
+
 # install software
+echo "Installing essential programs to work with cubian desktop"
+
 sudo apt-get update
 sudo apt-get install xfce4 ristretto geany iceweasel terminator 
 
-# install developement packages
+# install common development packages
+echo "Installing common development packages"
 sudo apt-get install git build-essential cmake
 
 # install nodejs
+echo "Installing nodejs"
 cd ~
 mkdir Node
 cd Node
@@ -18,13 +23,34 @@ make
 sudo make install
 
 # install libfreenect
+echo "Installing libfreenect"
 cd ~
+mkdir Kinect
+cd Kinect
 git clone https://github.com/OpenKinect/libfreenect
 cd libfreenect
 
 #install libfreenect deps
+echo "Installing libfreenect dependancies"
 sudo apt-get install libusb-1.0.0 freeglut3 freeglut3-dev libxmu-dev libxi-dev libgl1-mesa-dev
 
+#compile libfreenect
+echo "Compiling libfreenct"
 mkdir build
 cd build
 cmake ..
+make
+sudo make install
+
+
+# install openni
+
+echo "Installing Openni"
+cd ~/Kinect/
+git clone https://github.com/OpenNI/OpenNI
+echo "Please modify file /OpenNI/Platform/Linux/Build/Common/Platform.Arm and remove soft switch before compiling"
+
+
+# install openni deps 
+echo "Installing Openni dependancies"
+sudo apt-get install openjdk-6-jdk doxygen
