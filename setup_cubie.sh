1e8a16c05487da7375ef0679e53969df73f644f1
Peter Beno <najlepsiwebdesigner@gmail.com>
2014-01-13 12:01:41 +0000

    add pcl dependancies and build instructions

--- setup_cubie.sh
+++ setup_cubie.sh
@@ -68,10 +68,29 @@ echo "installing PCL"
 cd ~/Kinect/
 git clone https://github.com/PointCloudLibrary/pcl
 
+#install pcl deps
+echo "Installing PCL dependancies"
+sudo apt-get install doxygen
+sudo apt-get install libboost-all-dev
+sudo apt-get install libeigen3-dev
+sudo apt-get install libflann-dev
+sudo apt-get install python-sphinx
+sudo apt-get install libvtk5.8
+sudo apt-get install libvtk5-dev
+sudo apt-get install libqhull-dev
+sudo apt-get install libpng++-dev
+sudo apt-get install libvtk5.8-qt4 libvtk5.8 libvtk5-qt4-dev
+sudo apt-get install libgtest-dev
+sudo apt-get install qt-sdk
+sudo apt-get install libglew-dev
 
 
+# cmake
+cd ~/Kinect/pcl
+cmake -DCMAKE_BUILD_TYPE=Release .
 
 
+# now please manually modify file CMakeCache.txt and set cmake flags correctly to build visualizations and apps
 
 
 
